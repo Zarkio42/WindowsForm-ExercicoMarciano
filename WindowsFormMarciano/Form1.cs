b5e23f0478a405b838313aff39d443025b96083f
Pedro Cardoso <pedro.contato24@gmail.com>
2023-05-22 16:54:32 -0300

    subindo o game

---
+++ WindowsFormMarciano/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormMarciano
+{
+    public partial class Form1 : Form
+    {
+        Arvore arvore = new Arvore();
+        Marciano marciano = new Marciano();
+        Cacador cacador = new Cacador();
+        int contadorCliques;
+        Random random = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        public void button1_Click(object sender, EventArgs e)
+        {
+            FormWin win = new FormWin();
+            int valorUpdown = (int)numericUpDown1.Value;
+            
+            if (valorUpdown == marciano.posicao)
+            {
+                win.Show();
+                MessageBox.Show(marciano.acertou);
+            }
+            else
+            {
+                MessageBox.Show("Errou amigão!");
+            }
+        }
+
+        public void Form1_Load(object sender, EventArgs e)
+        {
+            marciano.posicao = random.Next(1, 10);            
+
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        public void clickbtn(object sender, MouseEventArgs e)
+        {
+            
+
+            Form2 form2 = new Form2();
+            cacador.bala--;
+
+            if (cacador.bala == 0)
+            {
+                form2.Show();
+                MessageBox.Show("Vou te pegar!");
+            }
+            
+        }
+    }
+}
---
+++ WindowsFormMarciano/Marciano.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormMarciano
+{
+    public class Marciano : Arvore
+    {
+        public string acertou = "Fui atingido!";
+        public string fim = "Vou te pegar!";
+        public string orientacao1 = "Estou mais a direita!", orientacao2 = "Estou mais a esquerda!";
+        public int posicao = 0;
+    }
+}
