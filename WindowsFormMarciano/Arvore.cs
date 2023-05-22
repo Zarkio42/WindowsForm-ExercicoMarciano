d17fd9c70b6b7f5fd9871dd2f23a36e628d1aa83
Pedro Cardoso <pedro.contato24@gmail.com>
2023-05-22 17:40:23 -0300

    subindo o game

---
+++ WindowsFormMarciano/Arvore.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormMarciano
+{
+    public class Arvore
+    {
+        public string arvoreSom = "Ugh!";
+        
+    }
+}
